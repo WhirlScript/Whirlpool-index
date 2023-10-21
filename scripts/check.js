e6c49cfe7bc24e0d5fab81344677f66e4c098d3d
serverbread <qjy20080319@yeah.net>
2023-10-21 22:28:15 +0800

    函数整合+历史版本

--- scripts/check.js
+++ scripts/check.js
@@ -20,12 +20,20 @@ const defaultInf = {
 
 // 这个函数如果成功返回一个Object，不成功返回null
 async function getRepoInf(name, url) {
+    
+}
+
+module.exports = async function write2bucket(packName, fileName) {
+    let packagesInf = JSON.parse(JSON.stringify(defaultInf));  // 将JSON拷贝
+    const packages = JSON.parse(fs.readFileSync('../packages.json').toString());
+    const url = packages[packName];
+
     const re = new RegExp(`https://github.com/[a-zA-Z0-9_-]+/[a-zA-Z0-9_-]+`);
     if (!re.test(url)) {
         return;
     }
     const repoPath = url.split(`https://github.com/`)[1];
-    const filePath = `../bucket/${name}.json`;
+    const filePath = `../bucket/${packages}.json`;
     let hasNewVersion = null;
 
     console.log('获取githubAPI')
@@ -66,36 +74,38 @@ async function getRepoInf(name, url) {
         hasNewVersion: hasNewVersion
     }
     //console.log(rawRes);
-    if (rawRes.name !== name) {
+    if (rawRes.name !== packages) {
         return null;
     }
     if (hasNewVersion) {
         let old = JSON.parse(fs.readFileSync(filePath));
         fs.writeFileSync(JSON.stringify(old.pop(data)));
-    } else {
+    }/* else {
         return data;
-    }
-}
+    }*/
 
-module.exports = async function write2bucket(packName, fileName) {
-    let packagesInf = JSON.parse(JSON.stringify(defaultInf));  // 将JSON拷贝
-    const packages = JSON.parse(fs.readFileSync('../packages.json').toString());
-    try {
-        getRepoInf(packName, packages[packName]).then(resolveCallback => {
-            // 生成json数据
-            //console.debug(resolveCallback)
-            packagesInf.name = packName;
-            packagesInf.repo = resolveCallback.repo;
-            packagesInf.versions[0].version = resolveCallback.version;
-            packagesInf.versions[0].sha1 = resolveCallback.sha;
+    packagesInf.name = packName;
+    packagesInf.repo = data.repo;
+    packagesInf.versions[1] = packagesInf.versions[0];
+    packagesInf.versions[0].version = data.version;
+    packagesInf.versions[0].sha1 = data.sha;
     
-            console.log(`${resolveCallback.version} ${resolveCallback.sha}`)
+    console.log(`${data.version} ${data.sha}`)
+    // 写入文件
+    packagesInf
+
+    fs.writeFileSync(`../bucket/${fileName}`, JSON.stringify(packagesInf));
+        /*
+        getRepoInf(packName, packages[packName]).then(data => {
+            // 生成json数据
+            //console.debug(data)
+            packagesInf.name = packName;
+            packagesInf.repo = data.repo;
+            packagesInf.versions[0].version = data.version;
+            packagesInf.versions[0].sha1 = data.sha;
+    
+            console.log(`${data.version} ${data.sha}`)
             // 写入文件
             fs.writeFileSync(`../bucket/${fileName}`, JSON.stringify(packagesInf));
-            }
-        );
-    } catch (e) {
-        console.error(e);
-    }
-    
+        });*/
 }
